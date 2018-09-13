35cbd3365f1888d0223393d67b37e5a5723cc303
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-13 20:07:49 +1200

    made search bar responsive and functional

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subject, of } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
+
+import { User } from '../user';
+
+import { AppointmentService } from '../appointment.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +13,29 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  users$: Observable<User[]>;
+  searching: boolean = false;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private apptService: AppointmentService) { }
 
-  ngOnInit() {
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  searchUsers(term: string): Observable<User[]> {
+    if (!term.trim()) {return of([])}
+    return this.apptService.getUsers().pipe(
+      map(users => users.filter(user => user.Name.toLowerCase().startsWith(term.toLowerCase())))
+    )
+  }
+
+  ngOnInit(): void {
+    this.users$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.searchUsers(term))
+    )
   }
 
 }

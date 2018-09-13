4e3ef20e664ff46128b35c436e939ddb88ffd43a
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-13 15:32:44 +1200

    finished appointment service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+//  { path: "", component: ComponentName  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }

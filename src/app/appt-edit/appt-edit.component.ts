3d8f22461befa1b8ad62119542aa3b9da2e99b55
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-14 17:24:38 +1200

    halfway through setting up time/date pickers

--- src/app/appt-edit/appt-edit.component.ts
+++ src/app/appt-edit/appt-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { AppointmentService } from '../appointment.service';
+import { NgbDateStruct, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+
+import { User } from '../user';
+import { Appointment } from '../appointment';
 
 @Component({
   selector: 'app-appt-edit',
@@ -7,9 +14,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApptEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location,
+    private apptService: AppointmentService
+  ) { }
+
+  appointment: Appointment;
+  people: User[];
+  addingNote: boolean;
+  date: NgbDateStruct;
+  startTime: NgbTimeStruct;
+  endTime: NgbTimeStruct;
+  @ViewChild('newNote') newNote;
 
   ngOnInit() {
+    this.getAppointment();
+  }
+
+  getAppointment(): void {
+    let Id: number;
+    this.route.params.subscribe(params => {
+       this.apptService.getAppointment(parseInt(params['Id']))
+        .subscribe(appt => {
+          this.appointment = appt;
+          this.setTimes();
+          this.getUsers();
+        })
+    });
+  }
+
+  setTimes(): void {
+    const start = this.appointment.Start;
+    const end = this.appointment.End;
+    this.date = {
+      day: start.getDate(),
+      month: start.getMonth(),
+      year: start.getFullYear()
+    };
+    this.startTime = {
+      hour: start.getHours(),
+      minute: start.getMinutes(),
+      second: start.getSeconds()
+    };
+    this.endTime = {
+      hour: end.getHours(),
+      minute: end.getMinutes(),
+      second: end.getSeconds()
+    };
+  }
+
+  getUsers(): void {
+    this.people = [];
+    for (let Id of this.appointment.Party) {
+      this.apptService.getUser(Id)
+        .subscribe(user => this.people.push(user))
+    }
+  }
+
+  addNote(): void {
+    const noteText = this.newNote.nativeElement.value;
+    this.appointment.Notes.push(noteText);
+    this.apptService.updateAppointment(this.appointment)
+      .subscribe(() => this.addingNote=false);
+  }
+
+  goBack(): void {
+    if (window.history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigate([''])
+    }
   }
 
 }

5a7b277dc14d076e7a926c3f307a1c68804c7536
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-14 16:40:01 +1200

    edit button works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserPageComponent } from './user-page/user-page.component';
 import { HomeComponent } from './home/home.component';
 import { ApptPageComponent } from './appt-page/appt-page.component';
+import { ApptEditComponent } from './appt-edit/appt-edit.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "u/:Id", component: UserPageComponent },
   { path: "a/:Id", component: ApptPageComponent },
-//  { path: "a/:Id/edit", component: ApptEditComponent }
+  { path: "a/:Id/edit", component: ApptEditComponent }
 ];
 
 @NgModule({

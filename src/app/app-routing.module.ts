99e8c69346a063fb001932a8c14f2bd8dab78bf9
Andre Tupara Morrison <andretuparamorrison@gmail.com>
2018-09-14 12:51:48 +1200

    implemented basic functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserPageComponent } from './user-page/user-page.component';
+import { ApptPageComponent } from './appt-page/appt-page.component';
 
 const routes: Routes = [
 //  { path: "", component: ComponentName  }
-  { path: "u/:Id", component: UserPageComponent }
+  { path: "u/:Id", component: UserPageComponent },
+  { path: "a/:Id", component: ApptPageComponent }
 ];
 
 @NgModule({
--- src/app/appt-page/appt-page.component.ts
+++ src/app/appt-page/appt-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { AppointmentService } from '../appointment.service';
+
+import { User } from '../user';
+import { Appointment } from '../appointment';
 
 @Component({
   selector: 'app-appt-page',
@@ -7,9 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApptPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private apptService: AppointmentService
+  ) { }
+
+  appointment: Appointment;
 
   ngOnInit() {
+    this.getAppointment();
+  }
+
+  getAppointment(): void {
+    let Id: number;
+    this.route.params.subscribe(params => {
+       this.apptService.getAppointment(parseInt(params['Id']))
+        .subscribe(appt => this.appointment = appt)
+    });
   }
 
 }
